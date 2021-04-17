28f6db30ed49575c1992baa3dee93c1e96a0c4f9
Alberto Passalacqua <apcfd@outlook.com>
2021-04-16 22:38:04 -0500

    PRJ: Updated headers and removed unused files.

--- dynamicSmagorinsky.C
+++ dynamicSmagorinsky.C
@@ -1,11 +1,11 @@
 /*---------------------------------------------------------------------------*\
 dynamicSmagorinsky - Implementation of the dynamic Smagorinsky
                      SGS model.
-    
+
 Copyright Information
     Copyright (C) 1991-2009 OpenCFD Ltd.
     Copyright (C) 2010-2021 Alberto Passalacqua 
-    
+
 License
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
